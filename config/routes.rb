4cde392fd739d2da18718630f2eb7494b7765349
ryan <ball.ryan.rb@gmail.com>
2021-10-01 14:43:06 -0600

    adds RESTful routes for expenses, incomes and monthly_balances

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,27 @@
 Rails.application.routes.draw do
+  # Expenses
+  get "/expenses" => "expenses#index"
+  post "/expenses" => "expenses#create"
+  get "/expenses" => "expenses#show"
+  patch "/expenses" => "expenses#update"
+  delete "/expenses" => "expenses#destroy"
+
+  # Incomes
+  get "/incomes" => "incomes#index"
+  post "/incomes" => "incomes#create"
+  get "/incomes" => "incomes#show"
+  patch "/incomes" => "incomes#update"
+  delete "/incomes" => "incomes#destroy"
+
+  # MonthlyBalances
+  get "/monthly_balances" => "monthly_balances#index"
+  post "/monthly_balances" => "monthly_balances#create"
+  get "/monthly_balances" => "monthly_balances#show"
+  patch "/monthly_balances" => "monthly_balances#update"
+  delete "/monthly_balances" => "monthly_balances#destroy"
+
+  # Account creation and login
   post "/users" => "users#create"
-  
   post "/sessions" => "sessions#create"
 
 end
