f1e3ca1105a2b64a1214ff04c123efd910ed9b78
ryan <ball.ryan.rb@gmail.com>
2021-11-20 15:39:35 -0700

    adds total and category_totals method to expense_group serializer

--- app/serializers/expense_group_serializer.rb
+++ app/serializers/expense_group_serializer.rb
@@ -1,5 +1,15 @@
 class ExpenseGroupSerializer < ActiveModel::Serializer
-  attributes :id, :name, :start_date, :end_date
+  attributes :id, :name, :start_date, :end_date, :total, :category_totals
 
   has_many :expenses
+
+  def total
+    object.expenses.sum { |expense| expense[:amount] }
+  end
+
+  def category_totals
+    category_hash = Hash.new(0)
+    object.expenses.each { |expense| category_hash[expense.category] += expense.amount }
+    category_hash
+  end
 end
