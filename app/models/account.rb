9a75b2672c83c09dcb19b520052bfa9d3b6ac7e0
ryan <ball.ryan.rb@gmail.com>
2022-01-20 21:54:11 -0700

    removes net_cash_calc validation

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,7 +3,6 @@ class Account < ApplicationRecord
   validates :account_identifier, presence: true
   validates :account_type, presence: true
   validates :accounting_type, presence: true
-  validates :net_cash_calc, presence: true
 
   belongs_to :user
   has_many :balances
