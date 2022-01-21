8255b140d1e01819d87a2f6540278136ff2b422a
ryan <ball.ryan.rb@gmail.com>
2022-01-20 20:05:23 -0700

    adds index and create actions to accounts controller

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,2 +1,22 @@
 class AccountsController < ApplicationController
+  def index
+    render json: Account.where(user_id: current_user.id).sort(:type)
+  end
+
+  def create
+    account = Account.new(
+      name: params[:name],
+      account_identifier: params[:account_identifier],
+      description: params[:description],
+      type: params[:type],
+      accounting_type: params[:accounting_type],
+      net_cash_calc: params[:net_cash_calc],
+      user_id: current_user.id,
+    )
+    if account.save
+      render json: account         #HAPPY PATH
+    else
+      render json: { errors: account.errors.full_messages }, status: :unprocessable_entity       #SAD PATH
+    end
+  end
 end
